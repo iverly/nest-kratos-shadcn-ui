b672c3ec79aa2c46ff1e76f1117cb4962a09b3eb
iverly <github@iverly.net>
2024-01-21 16:52:41 +0100

    feat(auth): add login flow for login page
    Signed-off-by: iverly <github@iverly.net>

--- src/app/auth/login/components/user-auth-form.tsx
+++ src/app/auth/login/components/user-auth-form.tsx
@@ -3,83 +3,93 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Icons } from "@/components/icons";
+import { Node } from "@/components/auth/node";
+
+import { frontend } from "@/lib/ory";
+import { LoginFlow, UpdateLoginFlowBody } from "@ory/client";
+import { filterNodesByGroups } from "@ory/integrations/ui";
+import { isAxiosError } from "axios";
+import { Message } from "@/components/auth/message";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
+  const [flow, setFlow] = React.useState<LoginFlow | undefined>(undefined);
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
-  async function onSubmit(event: React.SyntheticEvent) {
+  async function onSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     setIsLoading(true);
 
-    setTimeout(() => {
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    // map the entire form data to JSON for the request body
+    let body = Object.fromEntries(formData) as unknown as UpdateLoginFlowBody;
+
+    // extract the method from the submit button
+    if ("submitter" in event.nativeEvent) {
+      const method = (
+        event.nativeEvent as unknown as { submitter: HTMLInputElement }
+      ).submitter;
+      body = {
+        ...body,
+        ...{ [method.name]: method.value },
+      };
+    }
+
+    try {
+      await frontend.updateLoginFlow({
+        flow: flow!.id,
+        updateLoginFlowBody: body,
+      });
+    } catch (err: unknown) {
+      if (!isAxiosError(err)) {
+        throw err;
+      }
+
+      // handle the error
+      if (err.response?.status === 400) {
+        // user input error
+        // show the error messages in the UI
+        setFlow(err.response.data as LoginFlow);
+      }
+    } finally {
       setIsLoading(false);
-    }, 3000);
+    }
+  }
+
+  React.useEffect(() => {
+    frontend.createBrowserLoginFlow().then(({ data: flow }) => {
+      setFlow(flow);
+    });
+  }, []);
+
+  if (!flow) {
+    return (
+      <div className="flex justify-center">
+        <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+      </div>
+    );
   }
 
   return (
     <div className={cn("grid gap-6", className)} {...props}>
+      {flow.ui.messages?.map((message, index) => (
+        <Message key={index} text={message} />
+      ))}
+
       <form onSubmit={onSubmit}>
         <div className="grid gap-2">
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
-              Email
-            </Label>
-            <Input
-              id="email"
-              placeholder="name@example.com"
-              type="email"
-              autoCapitalize="none"
-              autoComplete="email"
-              autoCorrect="off"
-              disabled={isLoading}
-            />
-          </div>
-          <div className="grid gap-1">
-            <Label className="sr-only" htmlFor="email">
-              Password
-            </Label>
-            <Input
-              id="password"
-              placeholder="password"
-              type="password"
-              autoCapitalize="none"
-              autoComplete="password"
-              autoCorrect="off"
-              disabled={isLoading}
-            />
-          </div>
-          <Button disabled={isLoading}>
-            {isLoading && (
-              <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-            )}
-            Sign In with Email
-          </Button>
+          {filterNodesByGroups({
+            nodes: flow.ui.nodes,
+            groups: ["default", "password"],
+          }).map((node, index) => (
+            <Node key={index} node={node} loading={isLoading} />
+          ))}
         </div>
       </form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-      </div>
-      <Button variant="outline" type="button" disabled={isLoading}>
-        {isLoading ? (
-          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-        ) : (
-          <Icons.google className="mr-2 h-4 w-4" />
-        )}{" "}
-        Google
-      </Button>
     </div>
   );
 }
