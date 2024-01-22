4801957275acfdec69fccf27c32e1b250d352e9e
iverly <github@iverly.net>
2024-01-22 15:07:49 +0100

    feat(ui): add logout button
    Signed-off-by: iverly <github@iverly.net>

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 import { useSession } from "@/contexts/Session";
 
 export function Banner() {
-  const { session } = useSession();
+  const session = useSession();
 
-  if (!session) {
+  if (!session.data) {
     return (
       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
         Get started by&nbsp;
@@ -25,9 +25,12 @@ export function Banner() {
   return (
     <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
       You are logged in.&nbsp;
-      <Link href="/auth/logout">
-        <code className="font-mono font-bold">Click to Logout</code>
-      </Link>
+      <code
+        className="font-mono font-bold cursor-pointer"
+        onClick={() => session.destroy()}
+      >
+        Click to Logout
+      </code>
     </p>
   );
 }
--- src/contexts/Session.tsx
+++ src/contexts/Session.tsx
@@ -4,15 +4,18 @@ import React from "react";
 
 import { Session } from "@ory/client";
 import { frontend } from "@/lib/ory";
+import axios from "axios";
 
 interface SessionContextProps {
   data: Session | undefined;
   set: React.Dispatch<React.SetStateAction<Session | undefined>>;
+  destroy: () => void;
 }
 
 export const SessionContext = React.createContext<SessionContextProps>({
   data: undefined,
   set: () => {},
+  destroy: () => {},
 });
 
 export const SessionProvider = ({
@@ -28,8 +31,24 @@ export const SessionProvider = ({
     });
   }, []);
 
+  const destroy = React.useCallback(() => {
+    frontend
+      .createBrowserLogoutFlow()
+      .then(({ data: flow }) => {
+        return axios.get(flow.logout_url, {
+          withCredentials: true,
+        });
+      })
+      .then(() => {
+        setData(undefined);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
-    <SessionContext.Provider value={{ data, set: setData }}>
+    <SessionContext.Provider value={{ data, set: setData, destroy }}>
       {children}
     </SessionContext.Provider>
   );
